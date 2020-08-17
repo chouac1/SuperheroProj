934ba556492767f98f72758b37fe8cb232d810f3
Work & School <chouac@live.com>
2020-08-17 15:41:32 -0500

    INIT

---
+++ Superheros/Controllers/SuperheroController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Superheros.Data;
+using Superheros.Models;
+
+namespace Superheros.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        public ApplicationDbContext context;
+        public SuperheroController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: SuperheroController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SuperheroController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SuperheroController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SuperheroController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Superhero superhero) //add to to the database
+        {
+            context.Superheroes.Add(superhero);
+            context.SaveChanges();
+
+            try
+            {
+                return RedirectToAction(nameof(Index));               
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Superheros/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Superheros.Models;
+
+namespace Superheros.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Superhero> Superheroes { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public ApplicationDbContext()
+        {
+        }
+    }
+}
