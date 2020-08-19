6669b760f77bf1cc586f2d8b7f279fdd12ab1a3d
Work & School <chouac@live.com>
2020-08-18 22:36:52 -0500

    Theme, display names modified. Dev added a new dark theme to wwwroot -> lib -> bootstrap. The display names were all updated to reflect correct spacing and proper English spelling. Dev also updated the navagation for users to have the ability to click on the list of current heroes. TODO: Lastly, DEV needs to add a favorite superhero into the main index page.

--- Superheros/Models/Superhero.cs
+++ Superheros/Models/Superhero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,9 +13,17 @@ namespace Superheros.Models
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        [DisplayName("Alter Ego")]
         public string AlterEgo { get; set; }
+
+        [DisplayName("Primary Ability")]
         public string PrimaryAbility {get; set; }
+
+        [DisplayName("Secondary Ability")]
         public string SecondaryAbility { get; set; }
+
+        [DisplayName("Catchphrase")]
         public string CatchPhrase{ get; set; }
 
 
